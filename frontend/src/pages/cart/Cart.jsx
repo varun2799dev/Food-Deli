c55b5aa3ba2814d08d38e431e75268347bf63326
Varun Tadpatri <varuntadpatri@gmail.com>
2025-01-08 22:02:19 +0530

    orderItems change

--- frontend/src/pages/cart/Cart.jsx
+++ frontend/src/pages/cart/Cart.jsx
@@ -71,7 +71,7 @@ const Cart = () => {
       const payload = {
         userId,  // User ID from localStorage
         username,  // Username from localStorage
-        items: orderedItems,  // Array of ordered items from the cart
+        orderedItems,  // Array of ordered items from the cart
         totalAmount,  // Total amount calculated in the frontend
       };
 
